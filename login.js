4180ca8ec4cf3564c0d6ba953abb74d48d092ae4
Marshyy <marshmelloweeee@gmail.com>
2023-06-27 07:24:21 -0300

    adicionei os arquivos da atividade que precisava

---
+++ login.js
@@ -0,0 +1,17 @@
+//################
+//codigo de validação de formulário de login
+
+function Verificar(){
+    ?
+    let email = document.getElementById('LoginEmail').value;
+    let senha = document.getElementById('loginSenha').value;
+    console.log(email);
+    
+    if(!email || !senha){
+        alert("campos de preenchimento obrigatório. porfavor preencher");
+    }
+
+}
+
+
+//#############################################
