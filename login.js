c43480c503d7415bafc62f93bba13fa04532643a
Marshyy <marshmelloweeee@gmail.com>
2023-06-27 08:24:45 -0300

    adicionando location aula2.html

--- login.js
+++ login.js
@@ -2,13 +2,15 @@
 //codigo de validação de formulário de login
 
 function Verificar(){
-    
+
     let email = document.getElementById('LoginEmail').value;
     let senha = document.getElementById('loginSenha').value;
     console.log(email);
     
     if(!email || !senha){
         alert("campos de preenchimento obrigatório. porfavor preencher");
+    } else {
+        window.location.href = "aula2.html"
     }
 
 }
