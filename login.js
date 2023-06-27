4f0ab85c07f59e99c19df8e15e621ba28099bfdc
Marshyy <marshmelloweeee@gmail.com>
2023-06-27 07:52:41 -0300

    modifica arquivos

--- login.js
+++ login.js
@@ -2,7 +2,7 @@
 //codigo de validação de formulário de login
 
 function Verificar(){
-    ?
+    
     let email = document.getElementById('LoginEmail').value;
     let senha = document.getElementById('loginSenha').value;
     console.log(email);
