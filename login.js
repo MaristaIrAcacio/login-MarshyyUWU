027fddeabe975dae4795149262d2b8c4cdb2c160
Marshyy <marshmelloweeee@gmail.com>
2023-07-25 08:05:50 -0300

    tudo pronto

--- login.js
+++ login.js
@@ -30,21 +30,16 @@ function salvarUser()
 function acessar() {
 
     let loginEmail = document.getElementById("loginEmail").value;
-
     let loginSenha = document.getElementById("loginSenha").value;
 
- 
 
     if(!loginEmail || !loginSenha) {
-
         alert("favor preencha os campos para acessar o site");
 
     } else {
-
         window.location.href = "aula2.html";
 
     }
-
 }
 
 
@@ -53,10 +48,9 @@ function acessar() {
 var dadosLista = [];
 
 function salvarUser(){
-
   let nomeUser = document.getElementById("nomeUser").value;
-  if(nomeUser){
 
+  if(nomeUser){
     dadosLista.push(nomeUser);
 }
 
