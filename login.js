bc214df08140b040c8c06f1b829bf8cb43ae156d
Marshyy <marshmelloweeee@gmail.com>
2023-07-25 08:02:06 -0300

    comitando tudo que tem pós férias

--- login.js
+++ login.js
@@ -10,10 +10,55 @@ function Verificar(){
     if(!email || !senha){
         alert("campos de preenchimento obrigatório. porfavor preencher");
     } else {
-        window.location.href = "aula2.html"
+        window.location.href = "aula2.html";
+    }
+
+}
+
+// FUNÇÃO PARA ARMAZENAR NOMES EM ARRAY
+var dadosLista = [];
+
+function salvarUser()
+    let nomeUser = document.getElementById("NomeUser").value;
+
+    if(nomeUser){
+        dadosLista.push(nomeUser);
+    }
+
+    /* FUNÇÃO PARA VERIFICAR PREENCHIMENTO DE LOGIN*/
+
+function acessar() {
+
+    let loginEmail = document.getElementById("loginEmail").value;
+
+    let loginSenha = document.getElementById("loginSenha").value;
+
+ 
+
+    if(!loginEmail || !loginSenha) {
+
+        alert("favor preencha os campos para acessar o site");
+
+    } else {
+
+        window.location.href = "aula2.html";
+
     }
 
 }
 
 
+// FUNÇÃO PARA ARMAZENAR NOMES EM ARRAY
+
+var dadosLista = [];
+
+function salvarUser(){
+
+  let nomeUser = document.getElementById("nomeUser").value;
+  if(nomeUser){
+
+    dadosLista.push(nomeUser);
+}
+
+}
 //#############################################
