57ea7f55d531db2c500a5ac22247eb2f79abb722
Marshyy <marshmelloweeee@gmail.com>
2023-08-01 07:31:50 -0300

    lista em js feita

--- login.js
+++ login.js
@@ -1,6 +1,5 @@
 //################
 //codigo de validação de formulário de login
-
 function Verificar(){
 
     let email = document.getElementById('LoginEmail').value;
@@ -18,41 +17,35 @@ function Verificar(){
 // FUNÇÃO PARA ARMAZENAR NOMES EM ARRAY
 var dadosLista = [];
 
-function salvarUser()
-    let nomeUser = document.getElementById("NomeUser").value;
+function salvarUser(){
+    let nomeUser = document.getElementById("nomeUser").value;
 
     if(nomeUser){
         dadosLista.push(nomeUser);
-    }
-
-    /* FUNÇÃO PARA VERIFICAR PREENCHIMENTO DE LOGIN*/
-
-function acessar() {
-
-    let loginEmail = document.getElementById("loginEmail").value;
-    let loginSenha = document.getElementById("loginSenha").value;
-
-
-    if(!loginEmail || !loginSenha) {
-        alert("favor preencha os campos para acessar o site");
-
-    } else {
-        window.location.href = "aula2.html";
-
+        criarLista();
+        document.getElementById('nomeUser').value = '';
     }
 }
 
+/* FUNÇÃO PARA CRIAR LISTA DE USUÁRIO*/
+function criarLista(){
+    let tabela = document.getElementById('tabela').innerHTML =  "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
 
-// FUNÇÃO PARA ARMAZENAR NOMES EM ARRAY
-
-var dadosLista = [];
-
-function salvarUser(){
-  let nomeUser = document.getElementById("nomeUser").value;
-
-  if(nomeUser){
-    dadosLista.push(nomeUser);
+    for(let i = 0;i <= (dadosLista.length - 1);i++){
+        tabela+= "<tr><td>" + dadosLista[i] + "</td><td><button class='btn btn-success' onclick='Editar(this.parentNode.parentNode.rowIndex)'>Editar</button><button class='btn btn-danger' onclick='Excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
+        document.getElementById('tabela').innerHTML = tabela;
+    }
 }
 
-}
-//#############################################
+    /*FUNÇÃO PARA EDITAR NOME*/
+      function Editar(i){
+          document.getElementById('nomeUser').value = dadosLista[(i-1)];
+          dadosLista.splice(dadosLista[i-1], 1);
+    
+  }
+
+     function Excluir(i){
+         dadosLista.splice((i-1), 1);
+         document.getElementById('tabela').deleteRow(i);
+
+     }
